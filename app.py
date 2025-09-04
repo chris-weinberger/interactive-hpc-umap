57e7ff9ff5666782da09a916229ca02ed38afa9e
chris-weinberger <cleeweinberger@gmail.com>
2025-09-04 15:29:37 -0700

    change port

--- app.py
+++ app.py
@@ -239,4 +239,4 @@ def on_region_click(evt, selected_store):
 
 
 if __name__ == "__main__":
-    app.run_server(debug=True, host="127.0.0.1", port=8050)
+    app.run(debug=True, host='0.0.0.0', port=8080)
